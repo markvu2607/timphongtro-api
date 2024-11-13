689cb9577c59e137ef2b6ba209e5b725863d22bf
Mark Vu <markvu.work@gmail.com>
2024-11-13 20:02:13 +0700

    feat: implement api get me

--- src/api/users/users.service.ts
+++ src/api/users/users.service.ts
@@ -2,11 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserWithEmailResponseDto } from './dtos/responses/user-with-email.response.dto';
+import { Account } from '../auth/entities/account.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    @InjectRepository(Account)
+    private readonly accountsRepository: Repository<Account>,
   ) {}
+
+  async findOneById(id: number): Promise<UserWithEmailResponseDto> {
+    const account = await this.accountsRepository.findOne({
+      where: { user: { id: id } },
+      relations: ['user', 'role'],
+    });
+
+    return new UserWithEmailResponseDto(account);
+  }
 }
