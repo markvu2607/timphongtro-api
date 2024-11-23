f01ac1623d60da31c959464ca471940cd9e79d11
Mark Vu <markvu.work@gmail.com>
2024-11-23 13:31:44 +0700

    refactor: integrate docker & change bcrypt to bcryptjs (#9)

--- src/api/auth/hashing/bcrypt.service.ts
+++ src/api/auth/hashing/bcrypt.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { compare, genSalt, hash } from 'bcrypt';
+import { compare, genSalt, hash } from 'bcryptjs';
 import { HashingService } from './hashing.service';
 
 @Injectable()
 export class BcryptService implements HashingService {
   constructor(private readonly configService: ConfigService) {}
 
-  async hash(data: string | Buffer): Promise<string> {
+  async hash(data: string): Promise<string> {
     if (!data) {
       throw new Error('Data to hash cannot be empty!');
     }
@@ -17,7 +17,7 @@ export class BcryptService implements HashingService {
     return hash(data, salt);
   }
 
-  compare(data: string | Buffer, encrypted: string): Promise<boolean> {
+  compare(data: string, encrypted: string): Promise<boolean> {
     if (!data || !encrypted) {
       throw new Error('Both data and encrypted value are required!');
     }
